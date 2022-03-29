037723e96bd7e7e335ab899c596b802059561f69
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2022-03-28 21:34:43 -0300

    first commit

---
+++ src/main/java/com/gmartinez/bookshopping/Bookshopping.java
@@ -0,0 +1,13 @@
+package com.gmartinez.bookshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookshopping {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bookshopping.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmartinez/bookshopping/BookshoppingTests.java
@@ -0,0 +1,13 @@
+package com.gmartinez.bookshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshoppingTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
